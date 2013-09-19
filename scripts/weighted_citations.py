bf93a31e3e34df34c739459ffc4f1a8b3e0614f4
bendmorris <ben@bendmorris.com>
2013-09-19 15:18:10 -0400

    Ignore citations marked as low-confidence.

--- scripts/weighted_citations.py
+++ scripts/weighted_citations.py
@@ -2,6 +2,7 @@
 import sys
 input_path1 = sys.argv[1]
 input_path2 = sys.argv[2]
+from collections import defaultdict
 
 
 totals = {}
@@ -16,37 +17,28 @@ with open(input_path1) as input_file:
 
         totals[repo, dataset] = n
 
-reuse = {}
-no_reuse = {}
+reuse = defaultdict(lambda: 0)
+no_reuse = defaultdict(lambda: 0)
 with open(input_path2) as input_file:
     for line in input_file:
         if not line.strip(): continue
 
         n = int(line[:8])
         line = line[8:].rstrip('\n')
-        reuse_status, repo, dataset = line.split('\t')
+        confidence, reuse_status, repo = line.split('\t')
         if not repo.strip() or not dataset.strip(): continue
 
-        collection = None
-        if 'not reused' in reuse_status:
-            collection = no_reuse
+        if 'low' in confidence: pass
+        elif 'not reused' in reuse_status:
+            no_reuse[repo] += n
         elif 'reused' in reuse_status:
-            collection = reuse
-        if not collection is None:
-            if not (repo, dataset) in collection:
-                collection[(repo, dataset)] = 0
-            collection[(repo, dataset)] += n
+            reuse[repo] += n
 
-all_datasets = set.union(set(reuse.keys()), set(no_reuse.keys()), set(totals.keys()))
-for dataset in all_datasets:
+for repo, dataset in totals:
     # total number of citations for this dataset
-    total = totals.get(dataset, 0)
-    # number of subset that were marked as reuse
-    n = reuse.get(dataset, 0)
-    # total size of subset
-    subset = n + no_reuse.get(dataset, 0)
-    if subset == 0: freq = 1
-    else: freq = float(n)/subset
-    n = max(n, subset)
+    total = totals.get((repo, dataset), 0)
     # output repo, dataset, weighted estimate of reuse citations
-    print '\t'.join(dataset+(str(n * freq),))
+    print '\t'.join((repo, dataset)+(str(float(total) * reuse[repo] / no_reuse[repo]),))
+
+'''for repo in reuse:
+    print repo, reuse[repo], no_reuse[repo], float(reuse[repo]) / (reuse[repo] + no_reuse[repo])'''
