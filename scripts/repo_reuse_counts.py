a9109bcf6b2e78ac73c50f1fc0e3448d2d7830b7
bendmorris <ben@bendmorris.com>
2013-10-17 08:53:48 -0400

    For now, comment out these tests since they're testing the results of two different things.

--- scripts/repo_reuse_counts.py
+++ scripts/repo_reuse_counts.py
@@ -2,13 +2,13 @@
 '''Determines the number of citations within the subsample that were considered
 to have been instances of reuse with high confidence.
 
->>> test = {'HEPData': 154, 'ArrayExpress': 157, 'GEOROC': 170, 'Pangaea': 160, 'ICPSR/IQSS': 180, 'Journal Archives': 168, 'TreeBASE': 164, 'GEO': 162, 'PDB': 157, 'BMRB': 211}
->>> test_sorted = sorted(test.items(), key=lambda (a,b): b)
->>> data_sorted = sorted(candidate_counts.items(), key=lambda (a,b): b)
->>> ','.join([x[0] for x in data_sorted]) == ','.join([x[0] for x in test_sorted])
-'HEPData,ArrayExpress,PDB,Pangaea,GEO,TreeBASE,Journal Archives,GEOROC,ICPSR/IQSS,BMRB'
->>> all(abs(data_sorted[i][1] - test_sorted[i][1]) < 10 for i in range(len(test)))
-True
+#>>> test = {'HEPData': 154, 'ArrayExpress': 157, 'GEOROC': 170, 'Pangaea': 160, 'ICPSR/IQSS': 180, 'Journal Archives': 168, 'TreeBASE': 164, 'GEO': 162, 'PDB': 157, 'BMRB': 211}
+#>>> test_sorted = sorted(test.items(), key=lambda (a,b): b)
+#>>> data_sorted = sorted(candidate_counts.items(), key=lambda (a,b): b)
+#>>> ','.join([x[0] for x in data_sorted]) == ','.join([x[0] for x in test_sorted])
+#'HEPData,ArrayExpress,PDB,Pangaea,GEO,TreeBASE,Journal Archives,GEOROC,ICPSR/IQSS,BMRB'
+#>>> all(abs(data_sorted[i][1] - test_sorted[i][1]) < 10 for i in range(len(test)))
+#True
 '''
 from collections import defaultdict
 from process_dataset_list import clean_repo_name
