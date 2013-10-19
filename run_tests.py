47461988c613f6a2ce6f1f4fe78aa749228936b6
bendmorris <ben@bendmorris.com>
2013-10-18 21:50:47 -0400

    Run tests on all *.py files in scripts directory.

--- run_tests.py
+++ run_tests.py
@@ -4,20 +4,13 @@ import fnmatch
 import importlib
 
 
-def locate(pattern, root=os.curdir):
-    '''Locate all files matching supplied filename pattern in and below
-    supplied root directory.'''
-    for path, dirs, files in os.walk(os.path.abspath(root)):
-        for filename in fnmatch.filter(files, pattern):
-            yield os.path.join(path, filename)
-
 # run doctest unit tests in all source Python scripts
 failures = 0
-scripts = [x for x in locate('*.py', root='.')]
+scripts = [x for x in os.listdir('scripts') if x.endswith('.py')]
 for script in scripts:
     script = (os.path.relpath(script)[:-len('.py')]).replace('/', '.')
     print '** testing', script, '**'
-    mod = importlib.import_module(script)
+    mod = importlib.import_module('scripts.' + script)
     result = doctest.testmod(mod, verbose=True)
     failures += result.failed
 
