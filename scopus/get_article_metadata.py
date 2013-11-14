cf7a0c0f9a1471536eb53afd3b31b61bbf6f00e4
bendmorris <ben@bendmorris.com>
2013-11-14 12:07:17 -0500

    Always save on exit. KeyboardInterrupt isn't an instance of Exception.

--- scopus/get_article_metadata.py
+++ scopus/get_article_metadata.py
@@ -1,4 +1,5 @@
 import cPickle as pkl
+import spynner
 try:
     with open('data/article_metadata.pkl', 'r') as pkl_file:
         article_data = pkl.load(pkl_file)
@@ -13,19 +14,24 @@ bibdata = parser.parse_file('data/all.bib')
 
 try:
     for id in bibdata.entries:
+        print '==>', id
         if not id in article_data:
             b = bibdata.entries[id].fields
             try:
                 title = b['title'].strip('{}')
+                title = title.replace('{', '').replace('}', '')
+                if '^' in title: title = title.split('^')[-1]
                 article_data[id] = get_info_from_title(title)
             except KeyError: pass
             except NoMatchException:
                 print '** NO MATCH FOUND **'
+            except Exception as e:
+                print '** %s **' % e
 
     with open('data/article_metadata.pkl', 'w') as pkl_file:
         pkl.dump(article_data, pkl_file)
 
-except Exception as e:
+except:
     with open('data/article_metadata.pkl', 'w') as pkl_file:
         pkl.dump(article_data, pkl_file)
     raise
