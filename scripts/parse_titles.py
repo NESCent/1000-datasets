37dc497adae3de216cc46af1ac023e4acf270e6f
bendmorris <ben@bendmorris.com>
2013-11-20 11:21:10 -0500

    Better documentation of title parsing function.

--- scripts/parse_titles.py
+++ scripts/parse_titles.py
@@ -7,10 +7,14 @@ def main():
     import sys
     input = sys.stdin
     for line in input:
+        # file is repo [tab] accession [tab] reference
         vals = line.split('\t')
         repo, accession = vals[:2]
         ref = '\t'.join(vals[2:]).strip().replace('\n', '').replace('\r', '')
+        # split on various combinations of punctuation/spaces/capital letters 
+        # that seem to frequently delineate the title
         chunks = separator.split(ref)
+        # the title is probably the longest chunk after splitting this way
         title = sorted(chunks, key=lambda k: len(k), reverse=True)[0]
         print '\t'.join((repo, accession, title))
 
